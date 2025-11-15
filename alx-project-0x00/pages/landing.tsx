ffbbe4f1d1c512fbec16afc3f4d1fc23584204d7
CornellsWorld <corneliusalhassan@gmail.com>
2025-11-15 03:54:01 +0100

    update

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,13 @@
+const Landing: React.FC =  () => {
+  return (
+    <div>
+      <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <nav>
+        <ul>
+          
+        </ul>
+      </nav>
+    </div>
+  )
+}
+export default Landing
